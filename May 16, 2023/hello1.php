cfbad08a9cbd390f0494a1b0dc6e3245d28d4316
Daniel Viola <daniel.viola001@mymdc.net>
2023-05-23 18:13:10 -0400

    first assignment

---
+++ May 16, 2023/hello1.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello, world!</title>
+</head>
+<body>
+    <p>The following was created by PHP:
+        <?php print "Hello, world!"; ?>
+</p>
+</body>
+</html>
