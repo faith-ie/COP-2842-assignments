3aa307c8c95448fce3e0894131193bb516a6437d
Daniel Viola <daniel.viola001@mymdc.net>
2023-05-25 14:27:11 -0400

    oh wait the tags are closed in footer.php, im not used to doing it like this :skull:

--- May 25, 2023/summary.php
+++ May 25, 2023/summary.php
@@ -13,8 +13,7 @@
     $n = $_POST['name'];
     $d = $_POST['Description'];
     print "<h2> Thank you $n:</h2>\n<p style=\"font-size:120%\">Your request has been submitted.</p>\n\n<p>Summary of request:</p>\n<p>Type: $t</p>\n<p>Date and Time: $dt</p>\n<p>Description: $d</p>";
-   include "footer.php";
 ?>
-  </body>
-
-</html>
+<?php
+include "footer.php";
+?>
